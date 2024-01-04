f5967b7c5a7e1852f592cb0e3f8b04cc48c0b5d9
niinatui <niinatuikkanen@gmail.com>
2024-01-04 09:24:06 +0200

    teht 3.12

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://test:${password}@cluster0.ay0cso7.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+  
+const Person = mongoose.model('Person', personSchema)  
+
+if (process.argv.length<4) {
+    console.log('Phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+} else {
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+const person = new Person({
+  name: name,
+  number: number
+})
+
+person.save().then(result => {
+  console.log(`Added ${name} number ${number} to phonebook`)
+  mongoose.connection.close()
+})
+}
